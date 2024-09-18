b8feb9dba51e4cba7a0ae408efeaa6448a73224a
simonfisc <simon@mfisc.org>
2024-09-18 00:11:31 -0400

    Implement "task complete"

--- web.py
+++ web.py
@@ -14,7 +14,12 @@ st.subheader("This is my todo app.")
 st.write("This app is to increase your productivity")
 
 todos = functions.get_todos()
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
-st.text_input(label="", placeholder="Add a new todo...", on_change=add_todo, key="new_todo")
+st.text_input(label=".", label_visibility="hidden", placeholder="Add a new todo...", on_change=add_todo, key="new_todo")
